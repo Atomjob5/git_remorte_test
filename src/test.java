034b032485d446369dc3f6652841623f06fdc0af
Atom <424534558@qq.com>
2020-01-08 18:05:00 +0800

    commit 7

--- src/test.java
+++ src/test.java
@@ -12,4 +12,17 @@ public class test {
         String str = "github master writed";
         String str1 = "idea master writed";
     }
+
+    public static void say() {
+        System.out.println("dev-czk say");
+    }
+
+
+    public static void sit() {
+        System.out.println("dev-czk sit");
+
+
+    public static void fly(){
+        System.out.println("dev-czk fly");
+    }
 }
