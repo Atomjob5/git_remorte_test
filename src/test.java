5e9fdc47948230b3d7dce65d332949dac2cd20eb
Atom <424534558@qq.com>
2020-01-08 16:17:34 +0800

    commit 1

---
+++ src/test.java
@@ -0,0 +1,13 @@
+/**
+ * @author Atom
+ * @date 2020/1/8
+ * @time 16:16
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("2020年1月8日16:16:41");
+
+
+
+    }
+}
